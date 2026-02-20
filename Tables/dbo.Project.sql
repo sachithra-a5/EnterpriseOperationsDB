fd39f163ed0221c05130a36f134ad58e69ff2549
Sachithra Sahan <sachithra.sahan@ascentic.se>
2026-02-20 13:20:41 +0530

    3.6 Modify column

--- Tables/dbo.Project.sql
+++ Tables/dbo.Project.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[Project] (
     [CreatedDate]  DATETIME2(7)      NOT NULL CONSTRAINT [DF_dbo_Project_CreatedDate] DEFAULT (GETDATE()),
     [ModifiedDate] DATETIME2(7)      NULL,
     [IsActive]     BIT              NOT NULL CONSTRAINT [DF_dbo_Project_IsActive] DEFAULT (1),
-    [ProjectOwner] NVARCHAR(200) NULL, 
+    [ProjectOwner] NVARCHAR(250) NULL, 
     CONSTRAINT [PK_dbo_Project] PRIMARY KEY CLUSTERED ([ProjectId] ASC),
     CONSTRAINT [UQ_dbo_Project_ProjectCode] UNIQUE ([ProjectCode])
 );
